de64c25a1c834d088646a6876759ff251e3a549e
niceescap <niceescap@gmail.com>
2026-07-12 15:59:16 +0200

    Use poker T code for ten cards in backend deck

--- backend/deck.py
+++ backend/deck.py
@@ -5,7 +5,7 @@ import random
 
 class Deck:
     def __init__(self) -> None:
-        self.cards = [f"{rank}{suit}" for suit in "CDHS" for rank in ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")]
+        self.cards = [f"{rank}{suit}" for suit in "CDHS" for rank in ("A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K")]
         random.SystemRandom().shuffle(self.cards)
 
     def draw(self) -> str:
