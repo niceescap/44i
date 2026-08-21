8fd784b433ef7ab5803b738121d5ad5a88018bf8
niceescap <niceescap@gmail.com>
2026-08-21 23:10:24 +0200

    chore: bump appVersion 1.5.1+7

--- flutter_app/lib/api/rosace_api.dart
+++ flutter_app/lib/api/rosace_api.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import '../models/rosace_models.dart';
 
 class RosaceApi {
-  RosaceApi({http.Client? client, this.locale = 'fr', this.appVersion = '1.5.0+6'})
+  RosaceApi({http.Client? client, this.locale = 'fr', this.appVersion = '1.5.1+7'})
       : baseUrl = const String.fromEnvironment(
           'API_BASE_URL',
           defaultValue: 'https://44i.webredirect.org',
